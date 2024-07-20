288fb5d78b2b1879aceb4aa47d540e259ec4d916
Karan Soni <karansoni7973@gmail.com>
2024-07-21 00:00:36 +0530

    counter app created

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -1,33 +1,29 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
+  const incValue = ()=>{
+    setCount(count+1);
+  }
+
+  const decValue = ()=>{
+    if(count > 0)
+      setCount(count-1)
+  }
+
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>COUNTER : {count}</h1>
+      <button
+      onClick={incValue}
+      >increase</button>
+      <br />
+      <button
+      onClick={decValue}
+      >decrease</button>
     </>
   )
 }
